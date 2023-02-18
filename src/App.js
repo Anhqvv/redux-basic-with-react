a7a738a5e7870c624c387801cb29472600894bb2
Anhqvv <anhqvv2022@gmail.com>
2023-02-18 16:15:55 +0700

    #1 useHook with redux

--- src/App.js
+++ src/App.js
@@ -2,34 +2,32 @@ import React from 'react'
 
 import './App.css'
 
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
 
 import { increaseCounter, decreaseCounter } from './redux/action/action'
+import { useDispatch, useSelector } from 'react-redux'
 
 function App (props) {
+  const count1 = useSelector(state => {
+    return state.counter.count
+  })
+  const dispatch = useDispatch()
+  const handleIncrease = () => {
+    dispatch(increaseCounter())
+  }
+
+  const handleDescrease = () => {
+    dispatch(decreaseCounter())
+  }
   return (
     <div className='App'>
-      <div>Count: {props.count}</div>
+      <div>Count: {count1}</div>
 
-      <button onClick={() => props.increaseCounter1()}>Increase Count</button>
+      <button onClick={() => handleIncrease()}>Increase Count</button>
 
-      <button onClick={() => props.decreaseCounter1()}>Decrease Count</button>
+      <button onClick={() => handleDescrease()}>Decrease Count</button>
     </div>
   )
 }
 
-const mapStateToProps = state => {
-  return {
-    count: state.counter.count
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    increaseCounter1: () => dispatch(increaseCounter()),
-
-    decreaseCounter1: () => dispatch(decreaseCounter())
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,10 +3,11 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from '../reducer/rootReducer'
 
+// -----------------------------------------
 const store = createStore(
   rootReducer,
 
   composeWithDevTools()
 )
-
+// -----------------------------------------
 export default store
